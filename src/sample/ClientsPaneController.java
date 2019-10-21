a757abe153b888b7afe1ade9d5c466584873fcc7
Кирилл <kirillsafronov725@gmail.com>
2019-10-21 20:37:59 +0300

    some commit

--- src/sample/ClientsPaneController.java
+++ src/sample/ClientsPaneController.java
@@ -1,8 +1,14 @@
 package sample;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.io.IOException;
 
@@ -23,7 +29,17 @@ public class ClientsPaneController {
     @FXML
     void initialize() throws IOException {
         btnAddClient.setOnAction(event ->{
-
+            try {
+                FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("addClientPanel.fxml"));
+                Parent root1 = (Parent) fxmlLoader.load();
+                Stage stage = new Stage();
+                stage.initModality(Modality.APPLICATION_MODAL);
+                stage.setTitle("Добавление клиента");
+                stage.setScene(new Scene(root1));
+                stage.show();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         });
     }
 
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -10,7 +10,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("MainPage.fxml"));
+        Parent root =  FXMLLoader.load(getClass().getResource("MainPage.fxml"));
         primaryStage.setTitle("GymDB");
         primaryStage.setScene(new Scene(root, 1100, 700));
         primaryStage.show();
