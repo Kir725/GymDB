5fef8b27df9c2a669fadba9a22944326fcd653e8
kir-safronov@ya.ru <kir-safronov@ya.ru>
2019-10-22 17:10:01 +0300

    add addclintpane

--- src/sample/AddClientController.java
+++ src/sample/AddClientController.java
@@ -1,4 +1,38 @@
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
+
 public class AddClientController {
+
+    @FXML
+    private TextField tfSecondName;
+
+    @FXML
+    private TextField tfName;
+
+    @FXML
+    private TextField tfPatronymic;
+
+    @FXML
+    private TextField tfPhone;
+
+    @FXML
+    private TextField tfEmail;
+
+    @FXML
+    private DatePicker dpBornDate;
+
+    @FXML
+    private ChoiceBox<?> chbSex;
+
+    @FXML
+    private Button btnСancel;
+
+    @FXML
+    private Button btnSave;
+
 }
